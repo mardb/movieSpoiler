fa52dec7ac293905b36d93419a53af4bd426f66a
Marlene Guzman <marleneguzmanla@gmail.com>
2019-04-25 13:37:40 -0700

    descendant selectors

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 
-
-
-
-
+//hide the spoiler on page load
+$('.spoiler span').hide();
+//when the button is pressed,
+$('.spoiler button').click(function(){
+//show the spoiler text
+$('.spoiler span').show();
+//hide the 'reveal spoiler' button 
+$('.spoiler button').hide();
+// alert("i was clicked")
+});
